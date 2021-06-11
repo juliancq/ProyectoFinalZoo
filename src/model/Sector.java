03b6e4501339e5dd538358123ba1d525dfdeb9b8
Julian Castro <julmdq@live.com.ar>
2021-06-11 10:18:41 -0300

    Clase habitat

---
+++ src/model/Sector.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Sector<T> {
+	
+	private ArrayList<T> animales;
+	private float stockAlimento = 0;
+	private Date ultimaFechaMantenimiento = null;
+	private String nombreSector;
+	
+	public Sector() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Sector(String nombreSector) {
+		
+	}
+	
+	
+}
