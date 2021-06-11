b333d224ed6cd3bb7dd7bf88af8d72ab121fc825
Julian Castro <julmdq@live.com.ar>
2021-06-11 09:46:29 -0300

    Update Habitat.java

--- src/model/Habitat.java
+++ src/model/Habitat.java
@@ -1,5 +1,28 @@
 package model;
 
-public class Habitat {
+import java.util.ArrayList;
 
+public class Habitat {
+	
+	private float temperatura;
+	private String tipo;
+	private ArrayList<String> animales;
+	
+	public Habitat() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public float getTemperatura() {
+		return temperatura;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "HABITAT = " + getTipo();
+	}
 }
