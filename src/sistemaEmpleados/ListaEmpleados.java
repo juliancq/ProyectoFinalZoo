7ba4066a21313f7855ad0ff8ea03ae2d7ce64943
PablogRizzo <85520616+PablogRizzo@users.noreply.github.com>
2021-06-11 22:37:17 -0300

    Update listaEmpleados

--- src/sistemaEmpleados/ListaEmpleados.java
+++ src/sistemaEmpleados/ListaEmpleados.java
@@ -5,22 +5,33 @@ import java.util.ArrayList;
 public class ListaEmpleados 
 {
 	private ArrayList <Empleado> lista;
-	
+	/**
+	 * Lista a los empleados de la lista
+	 */
 	public ListaEmpleados() 
 	{
 		lista = new ArrayList<Empleado>();
 	}
-	
+	/**
+	 * Agrega un empleado a la lista
+	 * @param e
+	 */
 	public void addEmpleado(Empleado e)
 	{
 		lista.add(e);
 	}
-	
+	/**
+	 * Elimina un empleado de la lista
+	 * @param e
+	 */
 	public void removeEmpleado(Empleado e)
 	{
 		lista.remove(e);
 	}
-	
+	/**
+	 * Lista a todos los empleados de la lista
+	 * @return
+	 */
 	public String listado()
 	{
 		StringBuilder builder = new StringBuilder();
@@ -31,12 +42,18 @@ public class ListaEmpleados
 		}
 		return builder.toString();
 	}
-	
+	/**
+	 * Cuenta la cantidad de empleados de la lista
+	 * @return
+	 */
 	public int contar()
 	{
 		return lista.size();
 	}
-	
+	/**
+	 * Da de alta a un empleado segun el nro de legajo
+	 * @param legajo
+	 */
 	public void darDeAlta(int legajo)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -47,7 +64,10 @@ public class ListaEmpleados
 			}
 		}
 	}
-	
+	/**
+	 * Da de baja a un empleado segun el nro de legajo
+	 * @param legajo
+	 */
 	public void darDeBaja(int legajo)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -58,7 +78,11 @@ public class ListaEmpleados
 			}
 		}
 	}
-	
+	/**
+	 * Modifica el horario de un empleado
+	 * @param legajo
+	 * @param horario
+	 */
 	public void modificarHorario(int legajo, String horario)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -69,7 +93,11 @@ public class ListaEmpleados
 			}
 		}
 	}
-	
+	/**
+	 * Modifica el sueldo de un empleado
+	 * @param legajo
+	 * @param sueldo
+	 */
 	public void modificarSueldo(int legajo, double sueldo)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -80,7 +108,11 @@ public class ListaEmpleados
 			}
 		}
 	}
-	
+	/**
+	 * Modifica la direccion de un empleado
+	 * @param legajo
+	 * @param direccion
+	 */
 	public void modificarDireccion(int legajo, String direccion)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -91,7 +123,11 @@ public class ListaEmpleados
 			}
 		}
 	}
-	
+	/**
+	 * Modifica el telefono de un empleado
+	 * @param legajo
+	 * @param telefono
+	 */
 	public void modificarTelefono(int legajo, int telefono)
 	{
 		for(int i=0;i<lista.size();i++)
@@ -101,5 +137,35 @@ public class ListaEmpleados
 				lista.get(i).setTelefono(telefono);
 			}
 		}
+	}
+	/**
+	 * Elimina un empleado segun nro de legajo
+	 * @param legajo
+	 */
+	public void eliminarPorLegajo(int legajo)
+	{
+		for(int i=0;i<lista.size();i++)
+		{
+			if(lista.get(i).getLegajo() == legajo)
+			{
+				lista.remove(i);
+			}
+		}
+	}
+	/**
+	 * Busca un empleado segun el nro de legajo
+	 * @param legajo
+	 * @return
+	 */
+	public Empleado buscarPorLegajo(int legajo)
+	{
+		for(int i=0;i<lista.size();i++)
+		{
+			if(lista.get(i).getLegajo() == legajo)
+			{
+				return lista.get(i);
+			}
+		}
+		return null;
 	}
 }
